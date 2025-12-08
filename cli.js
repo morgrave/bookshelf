aef0960787d9b131a79f2c7a8583a32f579c614f
dwaldo <dwaldo@naver.com>
2025-12-08 14:11:48 +0900

    'deploy'

--- cli.js
+++ cli.js
@@ -3,8 +3,10 @@ const { exec } = require("child_process");
 
 const choices = [
   { name: "배포하기 (npm run deploy)", value: "npm run deploy" },
-  { name: "로그 스크립트 실행 (node log.js)", value: "node log.js" },
-  { name: "다운로드 스크립트 실행 (node download.js)", value: "node download.js" },
+  { name: "로그 다운로드 (node log.js)", value: "node log.js" },
+  { name: "로그 일괄 다운로드 (node download.js)", value: "node download.js" },
+  { name: "지식 베이스 파일 생성 (node KB.js)", value: "node KB.js" },
+  { name: "지식 베이스 업데이트 (node update.js)", value: "node update.js" },
 ];
 
 inquirer
